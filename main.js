71829a0edd04dcff5ba1e0381a6cd88436a9a042
Sirojiddin-1017 <74369205+Sirojiddin-1996@users.noreply.github.com>
2023-06-03 22:41:44 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -42,4 +42,23 @@ window.addEventListener('DOMContentLoaded', () => {
         slidesField.style.transform = `translateX(-${offset}px)`
     })
 
+
+    //========= ANIMATION =========//
+    window.addEventListener('scroll', reveal);
+
+    function reveal() {
+        var reveals = document.querySelectorAll('.reveal');
+
+        for (var i = 0; i < reveals.length; i++) {
+            var windowHeigth = window.innerHeight;
+            var revealTop = reveals[i].getBoundingClientRect().top;
+            var revealPoint = 100;
+
+            if (revealTop < windowHeigth - revealPoint) {
+                reveals[i].classList.add('active')
+            } else {
+                reveals[i].classList.remove('active')
+            }
+        }
+    }
 })
